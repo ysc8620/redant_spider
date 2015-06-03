0754d821a8c21a38d4c5b8789d3e4c80ee96c920
ysc8620 <ysc8620@163.com>
2015-06-03 18:04:54 +0800

    ds

--- kan360/kan360/common/imagepipelines.py
+++ kan360/kan360/common/imagepipelines.py
@@ -12,26 +12,25 @@ from base import *
 class MyImagesPipeline(ImagesPipeline):
     def file_path(self, request, response=None, info=None):
         #image_guid = request.url.split('/')[-1]
-        image_guid = hashlib.sha1(request.url).hexdigest()
+        #image_guid = hashlib.sha1(request.url).hexdigest()
         image_guid = str(binascii.crc32(request.url)).replace('-','0')
-        #path = image_guid[0:2]
+        path = image_guid[0:2]
         #date('is',time()) . tools_range::mackcode(3,'NS'). '.' . $ext;
         #time.time()
-        return '%s/%s/%s.jpg' % (time.strftime("%Y"),time.strftime("%m%d"), image_guid)
+        return '%s/%s/%s/%s.jpg' % (time.strftime("%Y"),time.strftime("%m%d"), path,image_guid)
 
     # 缩略图路径
     def thumb_path(self, request, thumb_id, response=None, info=None):
-        image_guid = hashlib.sha1(request.url).hexdigest()
+        #image_guid = hashlib.sha1(request.url).hexdigest()
         image_guid = str(binascii.crc32(request.url)).replace('-','0')
         path = image_guid[0:2] #thumbs
         #return 'full/%s%s.jpg' % (path, image_guid)
         #thumb_guid = hashlib.sha1(request.url).hexdigest()  # change to request.url after deprecation
-        return '%s/%s/%s/%s.jpg' % (time.strftime("%Y"),time.strftime("%m%d"),thumb_id,image_guid)
+        return '%s/%s/%s/%s/%s.jpg' % (time.strftime("%Y"),time.strftime("%m%d"),thumb_id,path,image_guid)
 
     def get_media_requests(self, item, info):
         try:
             for image_url in item['image_urls']:
-
                 yield Request(image_url)
         except Exception, e:
             print e
