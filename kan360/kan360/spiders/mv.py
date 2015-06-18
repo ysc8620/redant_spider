bfddc0746fa40463014fafce95b5a0a45cf1dfbe
ysc8620 <ysc8620@163.com>
2015-06-18 08:55:20 +0800

    ds

--- kan360/kan360/spiders/mv.py
+++ kan360/kan360/spiders/mv.py
@@ -24,8 +24,8 @@ class DmozSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow=r'/top/detail/((\?|&)(postype=\d+|toptype=\d+|topname=\d+)){0,3}$$')),
         Rule(LinkExtractor(allow=r'/topic/\w+.html$')),
-        Rule(LinkExtractor(allow=r'/gene/list.php((\?|&)(kw=[%\w]+|pageno=\d+){0,2}$')),
-        Rule(LinkExtractor(allow=r'/gene/mlist.php((\?|&)(kw=[%\w]+|pageno=\d+){0,2}$')),
+        Rule(LinkExtractor(allow=r'/gene/list.php((\?|&)(kw=[%\w]+|pageno=\d+)){0,2}$')),
+        Rule(LinkExtractor(allow=r'/gene/mlist.php((\?|&)(kw=[%\w]+|pageno=\d+)){0,2}$')),
 
         Rule(LinkExtractor(allow=r'/dianying/(\w+).html$')),
         # 提取匹配 'category.php' (但不匹配 'subsection.php') 的链接并跟进链接(没有callback意味着follow默认为True)
