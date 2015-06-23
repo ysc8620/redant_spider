92047e2ecc4b4bcb1864630119db0027adacd001
ysc8620 <ysc8620@163.com>
2015-06-23 21:23:01 +0800

    ds

--- kan360/run.sh
+++ kan360/run.sh
@@ -7,10 +7,6 @@ for file in `ls $basepath/kan360/websites/*.xml`
 do
  n=$(basename "${file%%.*}")
  scrapy crawl dmoz -a name=$n
- sleep 1m
+ #sleep 1m
 done
 #scrapy crawl dmoz -a n=imobshop.xml > /soft/py.log
-#sleep 5m
-#scrapy crawl dmoz -a n=deal.com.sg.xml > /soft/deal.log
-#sleep 5m
-#scrapy crawl dmoz -a n=deal.com.my.xml > /soft/mydeal.log
