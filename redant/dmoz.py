b2d595b43efa679360285c26c97e488b944bf5b5
ysc8620 <ysc8620@163.com>
2015-07-12 18:04:59 +0800

    dfs

--- redant/dmoz.py
+++ redant/dmoz.py
@@ -11,9 +11,9 @@ class myspider:
     allow_url = []
     def __init__(self):
         self.allow_url = [
-            # 'http://www.360kan.com/',
-            #  'http://www.360kan.com/dianying/index.html',
-            #  'http://www.360kan.com/dianshi/index.html',
+             'http://www.360kan.com/',
+              'http://www.360kan.com/dianying/index.html',
+              'http://www.360kan.com/dianshi/index.html',
              'http://www.360kan.com/dongman/index.html',
              'http://www.360kan.com/zongyi/index.html',
         ]
@@ -36,7 +36,7 @@ class myspider:
         splider=BrowserBase()
         for i in self.allow_url:
             data = splider.read(i)
-            html =  self.p(data.strip(),i)
+            html =  self.p(data,i)
             links = html.xpath("//a/@href")
             dt = datetime.now()
             d = dt.strftime( '%Y-%m-%d %H:%M' )
@@ -52,21 +52,23 @@ class myspider:
                         zy = zy - 0.001
                         print 'zy:', zy,' date:',d, ' url:', i
                         row = DB.init().update("UPDATE js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s", [zy,d,i])
+                        row = DB.init().update("UPDATE tiangua001.js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s", [zy,d,i])
                         #print 'zy:', zy, ' ', i
                     if "/m/" in i:
                         dy = dy - 0.001
                         print 'm: ', dy, ' date:',d, ' url:', i
                         row = DB.init().update("UPDATE js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s", [dy,d,i])
-
+                        row = DB.init().update("UPDATE tiangua001.js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s", [dy,d,i])
                     if "/tv/" in i:
                         tv = tv - 0.001
                         print 'tv: ', tv, ' date:',d, ' url:',
                         row = DB.init().update("UPDATE js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s", [tv,d,i])
-
+                        row = DB.init().update("UPDATE tiangua001.js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s", [tv,d,i])
                     if "/ct/" in i:
                         dm = dm - 0.001
                         print 'dm: ', dm,' date:',d, ' url:', i
                         row = DB.init().update("UPDATE js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s" ,[dm,d,i])
+                        row = DB.init().update("UPDATE tiangua001.js_vod SET sort=%s, 	vod_uptime=%s WHERE vod_reurl=%s" ,[dm,d,i])
 
 
 if __name__=='__main__':
